959544f5934e59add4b6b0a2e0c5f64b9d599b5b
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-05-31 19:19:15 +0300

    Sections 35 "Welcome to Section 3 "to 51 "Tagging game objects as Friendly"

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 5f;
+
+    Rigidbody rigidBody;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Thrust();
+        Rotate();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        { 
+            case "Friendly":
+            // Do nothing
+                print("OK"); // Remove
+                break;
+            case "Fuel":
+                print("Fuel"); // Remove
+                break;
+            default:
+                // Kill player
+                print("Dead");
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) //Can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)    // So it doesn't layer
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;  // Take manual control of rotation        
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;  // Resume physics control of rotation
+    }
+}
