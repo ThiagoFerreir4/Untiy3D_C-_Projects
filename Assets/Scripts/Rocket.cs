febf74523a916886db2306919322ce2924d11297
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-06-01 14:03:33 +0300

    60. Introducing Particle Effects

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Rocket : MonoBehaviour {
-
-    //To do fix lighting bug
+public class Rocket : MonoBehaviour
+{
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+
     [SerializeField] AudioClip mainEngine;
     [SerializeField] AudioClip success;
     [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
 
     Rigidbody rigidBody;
 	AudioSource audioSource;
@@ -26,7 +30,6 @@ public class Rocket : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        // Somewhere Stop Sound of Death
         if (state == State.Alive)
         {
             RespondToThrustInput();
@@ -57,6 +60,7 @@ public class Rocket : MonoBehaviour {
         state = State.Transcending;
         audioSource.Stop();
         audioSource.PlayOneShot(success);
+        successParticles.Play();
         Invoke("LoadNextLevel", 1f); // parameterise time
     }
 
@@ -65,6 +69,7 @@ public class Rocket : MonoBehaviour {
         state = State.Dying;
         audioSource.Stop();
         audioSource.PlayOneShot(death);
+        deathParticles.Play();
         Invoke("LoadFirstLevel", 1f); // parameterise time
     }
 
@@ -87,6 +92,7 @@ public class Rocket : MonoBehaviour {
         else
         {
             audioSource.Stop();
+            mainEngineParticles.Stop();
         }
     }
 
@@ -97,6 +103,7 @@ public class Rocket : MonoBehaviour {
         {
             audioSource.PlayOneShot(mainEngine);
         }
+        mainEngineParticles.Play();
     }
 
     private void  RespondToRotateInput()
