c8290ac838b8664f5410cab10770b4ada747ebfa
ThiagoFerreir4 <ferreirathiago.mail@gmail.com>
2020-06-01 13:33:07 +0300

    59. Playing Multiple Audio Clips. Long Rocket Thrust audio deleted

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,52 +3,86 @@ using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
+    //To do fix lighting bug
     [SerializeField] float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 5f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
 
     Rigidbody rigidBody;
 	AudioSource audioSource;
 
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 		rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
-	void Update () {
-        Thrust();
-        Rotate();
+	void Update ()
+    {
+        // Somewhere Stop Sound of Death
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) {  return; } // Ignore collisions when dead
+
         switch (collision.gameObject.tag)
         { 
             case "Friendly":
             // Do nothing
                 break;
             case "Finish":
-                print("Hit Finish!"); // Remove
-                SceneManager.LoadScene(1);
+                StartSuccessSequence();
                 break;
             default:
-                // Kill player  
-                print("Dead");
-                SceneManager.LoadScene(0);
+                StartDeathSequence();
                 break;
         }
     }
 
-    private void Thrust()
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", 1f); // parameterise time
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f); // parameterise time
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); //To do Allow for 2 more levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space)) //Can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying)    // So it doesn't layer
-            {
-                audioSource.Play();
-            }
+           ApplyThrust();
         }
         else
         {
@@ -56,7 +90,16 @@ public class Rocket : MonoBehaviour {
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)    // So it doesn't layer   
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void  RespondToRotateInput()
     {
         rigidBody.freezeRotation = true;  // Take manual control of rotation        
 
